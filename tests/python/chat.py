f38d1cf3b7ab7371016d84ef989c01b2d629b6c8
Nikhil Sonti <nikhilsv92@gmail.com>
2024-06-24 08:39:32 -0700

    renaming tests

---
+++ tests/python/chat.py
@@ -0,0 +1,28 @@
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    # This is the default and should be replaced with felafax API key
+    # api_key=os.environ.get("OPENAI_API_KEY"),
+    api_key=os.environ.get("FELAFAX_API_KEY"),
+
+    # default is https://api.openai.com/v1/
+    # base url for testing locally
+    # base_url="http://127.0.0.1:8000/v1/"
+    # base url for production instance
+    base_url = "https://felafax-proxy.shuttleapp.rs/v1/"
+
+)
+
+completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-3.5-turbo",
+)
+
+print(completion.choices[0].message.content)
+
